96788fd1b7ddaa1517c7ea7bd4729e6e63f2270e
bhm526@myy.haaga-helia.fi <bhm526@myy.haaga-helia.fi>
2026-08-24 01:21:00 +0300

    melkein valmis

--- laskin/app/(tabs)/index.tsx
+++ laskin/app/(tabs)/index.tsx
@@ -1,98 +1,74 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { TextInput, View, Text, TouchableOpacity } from 'react-native';
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+export default function Calculator() {
+  const [number1, setNumber1] = useState('');
+  const [number2, setNumber2] = useState('');
+  const [result, setResult] = useState<number | null>(null);
+  const [error, setError] = useState('');
 
-export default function HomeScreen() {
+  const calculate = (operation: '+' | '-') => {
+    if (number1.trim() === '' || number2.trim() === '') {
+      setError('Syötä molempien kentille numero.');
+      setResult(null);
+      return;
+    }
+
+    const num1 = Number(number1);
+    const num2 = Number(number2);
+
+    if (isNaN(num1) || isNaN(num2)) {
+      setError('Syötä vain numero.');
+      setResult(null);
+      return;
+    }
+
+    setError('');
+
+    if (operation === '+') {
+      setResult(num1 + num2);
+    } else {
+      setResult(num1 - num2);
+    }
+  };
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+    <View>
+      <Text>Laskin</Text>
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      <TextInput
+        placeholder='Ensimmäinen numero'
+        keyboardType='numeric'
+        value={number1}
+        onChangeText={setNumber1}
+      />
+
+      <TextInput
+        placeholder='Toinen numero'
+        keyboardType='numeric'
+        value={number2}
+        onChangeText={setNumber2}
+      />
+
+      <View>
+        <TouchableOpacity
+          onPress={() => calculate('+')}
+        >
+          <Text>+</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+        onPress={() => calculate('-')}
+        >
+          <Text>-</Text>
+        </TouchableOpacity>
+      </View>
+
+      {error !== '' && <Text>{error}</Text>}
+
+      {result !== null && (
+        <Text>Tulos: {result}</Text>
+      )}
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
